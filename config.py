8def3e45cd3f8c574965c73cb66ed750f39118ac
Egor Aksenov <anonaso3011@gmail.com>
2026-06-26 22:40:47 +0300

    -

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ developerMode = True
 adminChat = '-1002447798028'
 technicAccount = '7990032679'
 
-regions = ["Калмыкия 🌷", 'Дагестан 🏔', "Стамбул 🇹🇷", 'Алтай 🌸', "Карелия 🌲"]
-mes_ID_PDF = [2772, 2444, 2577, 2363, 3094]
+regions = ['Дагестан 🏔', 'Алтай 🌸', "Карелия 🌲"]
+mes_ID_PDF = [2444, 2363, 3094]
 startmes = 3090
 BOT_TOKEN = '7208352488:AAHChq9Q3z3H6rFoBrJCY2hz7W3qOsCuAmE'
 url_Chanel = 'https://t.me/mary_travel_russia'
